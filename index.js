d03d595b5eac2bce75226591b74120caba66cb07
j_h <13113017500>
2019-10-11 16:50:35 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+// 初始化注册组件
+AppRegistry.registerComponent(appName, () => App);
